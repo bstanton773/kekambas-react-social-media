b57bd23eacad5ddff1bda85c4613563ff32491fb
Brian <bstanton773@gmail.com>
2023-04-10 18:47:35 -0500

    Add pages to react app

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -4,15 +4,18 @@ import PostCard from '../components/PostCard';
 export default function Home({ user }) {
 
     const [posts, setPosts] = useState([]);
+    const [page, setPage] = useState(0);
 
     useEffect(() => {
         async function fetchPostData(){
             let response = await fetch('https://kekambas-blog-api.onrender.com/api/posts');
             let data = await response.json();
-            setPosts(data);
+            let firstIndex = page * 10;
+            let secondIndex = firstIndex + 10;
+            setPosts(data.slice(firstIndex, secondIndex));
         }
         fetchPostData();
-    }, []);
+    }, [page]);
 
     return (
         <div className='row'>
@@ -32,6 +35,13 @@ export default function Home({ user }) {
                                     <input type='submit' className='btn btn-primary' value='Search' />
                                 </div>
                             </form>
+                        </div>
+                    </div>
+                    <div className="card mt-3">
+                        <div className="card-header">Page: {page + 1}</div>
+                        <div className="card-body">
+                            { page > 0 ? (<button className="btn btn-danger w-50" onClick={() => setPage(page - 1)}>Page Down</button>) : null }
+                            { posts.length === 10 ? (<button className="btn btn-success w-50" onClick={() => setPage(page + 1)}>Page Up</button>) : null}
                         </div>
                     </div>
                 </div>
